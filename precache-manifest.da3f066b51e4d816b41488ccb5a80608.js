2e543d84d72b3ff674cdb47cf58bedc83d0fadb7
Keesha <khawki00@hotmail.com>
2020-09-09 22:16:30 -0400

    Updates

---
+++ precache-manifest.da3f066b51e4d816b41488ccb5a80608.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40421101c7cc4867c8b0a961c75b23a1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b2ec8664825adc8eb030",
+    "url": "/monsters-rolodex/static/css/main.de975126.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ec8664825adc8eb030",
+    "url": "/monsters-rolodex/static/js/main.6ecf1a63.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
